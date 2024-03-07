c119ea2094a0e1721de77ba2ec99751c08f3f831
AleDC91 <ale91.dallas@gmail.com>
2024-03-07 15:01:46 +0100

    prova

--- esercizio7marzo/app/Providers/RouteServiceProvider.php
+++ esercizio7marzo/app/Providers/RouteServiceProvider.php
@@ -35,6 +35,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->prefix('activity')
+                ->group(base_path('routes/activity.php'));
         });
     }
 }
